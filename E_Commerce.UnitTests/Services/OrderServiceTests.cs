866a6d8adcdd3bfc3e5e531282671359089d62b7
Abdallah Elshenawy <abdallah.elshenawy097@gmail.com>
2026-06-09 01:02:26 +0300

    Adding the unit tests

---
+++ E_Commerce.UnitTests/Services/OrderServiceTests.cs
@@ -0,0 +1,163 @@
+﻿
+using AutoMapper;
+using E_Commerce.Application.Common;
+using E_Commerce.Application.DTOs.OrderDTOs;
+using E_Commerce.Application.Interfaces;
+using E_Commerce.Application.Services;
+using E_Commerce.Domain.Entities;
+using E_Commerce.Domain.Exceptions;
+using FluentAssertions;
+using Moq;
+using System;
+
+namespace E_Commerce.UnitTests.Services
+{
+    public class OrderServiceTests
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+        private readonly Mock<IMapper> _mapperMock;
+        private readonly OrderService _orderService;
+        public OrderServiceTests()
+        {
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _mapperMock = new Mock<IMapper>();
+            _orderService = new OrderService(_unitOfWorkMock.Object, _mapperMock.Object);
+        }
+
+        [Fact]
+        public async Task PlaceOrderAsync_TheCustomerNotFound_ThrowsNotFoundException()
+        {
+            
+            _unitOfWorkMock.Setup(u => u.CustomerRepository.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((Customer?)null);
+
+            
+            await Assert.ThrowsAsync<NotFoundException>(async () => await _orderService.PlaceOrderAsync(new PlaceOrderDto(), 10));
+        }
+
+        [Fact]
+        public async Task PlaceOrderAsync_TheProductNotFound_ThrowsNotFoundException()
+        {
+            
+            Customer customer = new Customer("fname", "sname", "email");
+            _unitOfWorkMock.Setup(u => u.CustomerRepository.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((customer));
+
+            List<Product> products = new List<Product>
+            {
+                new Product("product1", "desc", 100m, 20) { Id = 1 },
+                new Product("product2", "desc", 100m, 20) { Id = 2 },
+            };
+            _unitOfWorkMock.Setup(u => u.ProductRepository.GetProductsByIdsAsync(It.IsAny<List<int>>())).ReturnsAsync(products);
+
+            PlaceOrderDto placeOrderDto = new PlaceOrderDto
+            {
+                Items = new List<OrderItemRequestDto>
+                {
+                    new OrderItemRequestDto
+                    {
+                        ProductId = 5,
+                        Quantity = 8
+                    },
+                    new OrderItemRequestDto
+                    {
+                        ProductId = 6,
+                        Quantity = 10
+                    },
+                }
+            };
+
+            
+            await Assert.ThrowsAsync<NotFoundException>(async () => await _orderService.PlaceOrderAsync(placeOrderDto, 1));
+        }
+
+        [Fact]
+        public async Task PlaceOrderAsync_TheQuantityIsInsufficient_ThrowsInsufficientStockException()
+        {
+            
+            Customer customer = new Customer("fname", "sname", "email");
+            _unitOfWorkMock.Setup(u => u.CustomerRepository.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((customer));
+            List<Product> products = new List<Product>
+            {
+                new Product("product1", "desc", 100m, 20) { Id = 1 },
+                new Product("product2", "desc", 100m, 20) { Id = 2 },
+            };
+            _unitOfWorkMock.Setup(u => u.ProductRepository.GetProductsByIdsAsync(It.IsAny<List<int>>())).ReturnsAsync(products);
+            PlaceOrderDto placeOrderDto = new PlaceOrderDto
+            {
+                Items = new List<OrderItemRequestDto>
+                {
+                    new OrderItemRequestDto
+                    {
+                        ProductId = 1,
+                        Quantity = 25
+                    },
+                    new OrderItemRequestDto
+                    {
+                        ProductId = 6,
+                        Quantity = 10
+                    },
+                }
+            };
+
+            
+            await Assert.ThrowsAsync<InsufficientStockException>(async () => await _orderService.PlaceOrderAsync(placeOrderDto, 1));
+        }
+
+        [Fact]
+        public async Task PlaceOrderAsync_CreatingOrder_OrderCreatedAndStockDecreasedAndSaveChangesOnce()
+        {
+            
+            Customer customer = new Customer("fname", "sname", "email");
+            _unitOfWorkMock.Setup(u => u.CustomerRepository.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(customer);
+            List<Product> products = new List<Product>
+            {
+                new Product("product1", "desc", 100m, 20) { Id = 1 },
+                new Product("product2", "desc", 100m, 20) { Id = 2 },
+            };
+            _unitOfWorkMock.Setup(u => u.ProductRepository.GetProductsByIdsAsync(It.IsAny<List<int>>())).ReturnsAsync(products);
+            _unitOfWorkMock.Setup(u => u.OrderRepository.Add(It.IsAny<Order>()));
+            PlaceOrderDto placeOrderDto = new PlaceOrderDto()
+            {
+                Items = new List<OrderItemRequestDto>()
+                {
+                    new OrderItemRequestDto() {ProductId = 1, Quantity = 10},
+                    new OrderItemRequestDto() {ProductId = 2, Quantity = 8}
+                }
+            };
+
+            await _orderService.PlaceOrderAsync(placeOrderDto, customer.Id);
+            
+            _unitOfWorkMock.Verify(u => u.OrderRepository.
+            Add(It.Is<Order>(o => 
+                o.OrderItems.Count == 2 &&
+                o.OrderItems.Any(i => i.ProductId == 1 && i.Quantity == 10) &&
+                o.OrderItems.Any(i => i.ProductId == 2 && i.Quantity == 8))), Times.Once);
+            products[0].StockQuantity.Should().Be(10);
+            products[1].StockQuantity.Should().Be(12);
+            _unitOfWorkMock.Verify(u => u.SaveChangesAsync(), Times.Once);
+
+        }
+
+        [Fact]
+        public async Task GetOrderByIdAsync_OrderIsNotFound_ReturnsNull()
+        {
+            _unitOfWorkMock.Setup(u => u.OrderRepository.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((Order?)null);
+
+            var actual = await _orderService.GetOrderByIdAsync(5);
+
+            actual.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetOrderByIdAsync_OrderExist_ReturnsValidOrderDto()
+        {
+            Order order = new Order(1);
+            _unitOfWorkMock.Setup(u => u.OrderRepository.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(order);
+            _mapperMock.Setup(m => m.Map<OrderDto>(It.IsAny<Order>())).Returns(new OrderDto());
+
+            var actual = await _orderService.GetOrderByIdAsync(5);
+
+            actual.Should().NotBeNull();
+
+        }
+    }
+}
---
+++ E_Commerce.UnitTests/Services/ProductServiceTests.cs
@@ -0,0 +1,197 @@
+﻿
+using AutoMapper;
+using E_Commerce.Application.Common;
+using E_Commerce.Application.DTOs.ProductDTOs;
+using E_Commerce.Application.Interfaces;
+using E_Commerce.Application.Services;
+using E_Commerce.Domain.Entities;
+using FluentAssertions;
+using Moq;
+
+namespace E_Commerce.UnitTests.Services
+{
+    public class ProductServiceTests
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+        private readonly Mock<ICacheService> _cacheMock;
+        private readonly Mock<IMapper> _mapperMock;
+        private readonly ProductService _productService;
+        public ProductServiceTests()
+        {
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _cacheMock = new Mock<ICacheService>();
+            _mapperMock = new Mock<IMapper>();
+            _productService = new ProductService(_unitOfWorkMock.Object, _cacheMock.Object, _mapperMock.Object);
+        }
+
+        [Fact]
+        public async Task GetProductById_CacheHit_ReturnsValidProductDto()
+        {
+            
+            ProductDto productDto = new ProductDto()
+            {
+                Id = 1, Description = "desc", Name = "product", Price = 100m, StockQuantity = 20
+            };
+            _cacheMock.Setup(c => c.GetAsync<ProductDto>(CacheKeys.Product(productDto.Id))).ReturnsAsync(productDto);
+            
+            ProductDto? actual = await _productService.GetProductByIdAsync(productDto.Id);
+
+            
+            _unitOfWorkMock.Verify(u => u.ProductRepository.GetByIdAsync(It.IsAny<int>()), Times.Never);
+            actual.Should().Be(productDto);
+        }
+
+        [Fact]
+        public async Task GetProductById_CacheMissAndProductDoesntExist_ReturnsNull()
+        { 
+            _cacheMock.Setup(c => c.GetAsync<ProductDto>(It.IsAny<string>())).ReturnsAsync((ProductDto?)null);
+            _unitOfWorkMock.Setup(u => u.ProductRepository.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((Product?)null);
+
+            
+            ProductDto? actual = await _productService.GetProductByIdAsync(10);
+
+            
+            actual.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetProductById_CacheMissAndProductExist_ReturnsProductDto()
+        {
+            Product product = new Product("product", "desc", 100m, 20);
+            _cacheMock.Setup(c => c.GetAsync<ProductDto>(It.IsAny<string>())).ReturnsAsync((ProductDto?)null);
+            _unitOfWorkMock.Setup(u => u.ProductRepository.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(product);
+            _mapperMock.Setup(m => m.Map<ProductDto>(product)).Returns(new ProductDto());
+            
+            ProductDto? actual = await _productService.GetProductByIdAsync(10);
+
+            
+            actual.Should().NotBeNull();
+        }
+
+
+        [Fact]
+        public async Task GetProductById_CacheMiss_SetCache()
+        {
+            
+            
+            Product product = new Product("product", "desc", 100m, 20);
+            _cacheMock.Setup(c => c.GetAsync<ProductDto>(It.IsAny<string>())).ReturnsAsync((ProductDto?)null);
+            _unitOfWorkMock.Setup(u => u.ProductRepository.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(product);
+            _mapperMock.Setup(m => m.Map<ProductDto>(product)).Returns(new ProductDto());
+
+            ProductDto? actual = await _productService.GetProductByIdAsync(10);
+
+            
+            _cacheMock.Verify(c => c.SetAsync(It.IsAny<string>(), It.IsAny<ProductDto>(), null), Times.Once);
+        }
+
+        [Fact]
+        public async Task CreateProductAsync_ValidateTheProductProperties_CreateProductWithValidProps()
+        {
+            _unitOfWorkMock.Setup(u => u.ProductRepository.Add(It.IsAny<Product>()));
+            CreateProductDto createProductDto = new CreateProductDto()
+            {
+                Description = "desc", Name = "product", Price = 100m, StockQuantity = 20
+            };
+            Product product = new Product("product", "desc", 100m, 20);
+            ProductDto productDto = new ProductDto()
+            {
+                Description = "desc", Name = "product", Price = 100m, StockQuantity = 20
+            };
+            _mapperMock.Setup(m => m.Map<ProductDto>(It.IsAny<Product>())).Returns(productDto);
+
+
+            var actual = await _productService.CreateProductAsync(createProductDto);
+
+            
+            actual.Name.Should().Be(createProductDto.Name);
+            actual.Description.Should().Be(createProductDto.Description);
+            actual.Price.Should().Be(createProductDto.Price);
+            actual.StockQuantity.Should().Be(createProductDto.StockQuantity);
+        }
+
+        [Fact]
+        public async Task CreateProductAsync_VerifyingAddAndSaveAndRemoveCache_AddAndSaveAndCacheShouldBeCalledOnce()
+        {
+            
+            
+            _unitOfWorkMock.Setup(u => u.ProductRepository.Add(It.IsAny<Product>()));
+            CreateProductDto createProductDto = new CreateProductDto()
+            {
+                Description = "desc",
+                Name = "product",
+                Price = 100m,
+                StockQuantity = 20
+            };
+
+            
+            var actual = await _productService.CreateProductAsync(createProductDto);
+
+            
+            _unitOfWorkMock.Verify(u => u.ProductRepository.Add(It.IsAny<Product>()), Times.Once);
+            _unitOfWorkMock.Verify(u => u.SaveChangesAsync(), Times.Once);
+            _cacheMock.Verify(c => c.RemoveAsync(It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateProductAsync_ProductIsNull_ThrowsNotFoundException()
+        {
+            
+            _unitOfWorkMock.Setup(u => u.ProductRepository.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((Product?)null);
+
+            
+            await Assert.ThrowsAsync<NotFoundException>(async () => await _productService.UpdateProductAsync(99, new UpdateProductDto()));
+        }
+
+        [Fact]
+        public async Task UpdateProductAsync_ProductIsFound_UpdateAndSaveAreCalledOnceAndRemoveCacheCalledTwice()
+        {
+            
+            Product product = new Product("product", "desc", 100m, 20);
+            _unitOfWorkMock.Setup(u => u.ProductRepository.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(product);
+            _unitOfWorkMock.Setup(u => u.ProductRepository.Update(It.IsAny<Product>()));
+
+            
+            UpdateProductDto productDto = new UpdateProductDto()
+            {
+                Description = "desc",
+                Name = "product",
+                Price = 100m
+            };
+            await _productService.UpdateProductAsync(10, productDto);
+
+            
+            _unitOfWorkMock.Verify(u => u.ProductRepository.Update(It.IsAny<Product>()), Times.Once);
+            _unitOfWorkMock.Verify(u => u.SaveChangesAsync(), Times.Once);
+            _cacheMock.Verify(c => c.RemoveAsync(It.IsAny<string>()), Times.Exactly(2));
+        }
+
+        [Fact]
+        public async Task DeleteProductAsync_ProductIsNull_ThrowsNotFoundException()
+        {
+            
+            _unitOfWorkMock.Setup(u => u.ProductRepository.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((Product?)null);
+
+            
+            await Assert.ThrowsAsync<NotFoundException>(async () => await _productService.DeleteProductAsync(10));
+        }
+
+        [Fact]
+        public async Task DeleteProductAsync_ProductIsFound_DeleteAndSaveCalledOnceAndRemoveCacheCalledTwice()
+        {
+            
+            
+            Product product = new Product("product", "desc", 100m, 20);
+            _unitOfWorkMock.Setup(u => u.ProductRepository.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(product);
+            _unitOfWorkMock.Setup(u => u.ProductRepository.Delete(It.IsAny<Product>()));
+
+            
+            await _productService.DeleteProductAsync(10);
+
+            
+            _unitOfWorkMock.Verify(u => u.ProductRepository.Delete(It.IsAny<Product>()), Times.Once);
+            _unitOfWorkMock.Verify(u => u.SaveChangesAsync(), Times.Once);
+            _cacheMock.Verify(c => c.RemoveAsync(It.IsAny<string>()), Times.Exactly(2));
+        }
+    }
+}
