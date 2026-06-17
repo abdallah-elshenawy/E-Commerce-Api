739f1fa2772958cfe759ace2608f320cab789d5f
Abdallah Elshenawy <abdallah.elshenawy097@gmail.com>
2026-06-17 03:00:11 +0300

    Adding the category entity

---
+++ E_Commerce.Domain/Entities/Category.cs
@@ -0,0 +1,32 @@
+﻿
+using E_Commerce.Domain.Exceptions;
+
+namespace E_Commerce.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        private string _name;
+        private Category() { }
+        public Category(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = !string.IsNullOrWhiteSpace(value) ? value
+                        : throw new DomainException("The name cannot be null or empty.");
+            }
+        }
+        public string Description { get; private set; }
+        public ICollection<Product> Products { get; private set; } = new List<Product>();
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+    }
+}
